85d7170b62b028d547672d6b5d40bc15fe521ca8
Samiha87 <samihajebril10@gmail.com>
2021-08-26 18:31:36 +0200

    restaurant application

---
+++ next-react-hooks-strapi/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9157e5838151d059062e207cb02225e8'),
+    },
+  },
+});
---
+++ next-react-hooks-strapi/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1058eea0-cbe2-4c04-a46c-8107f51d7bab'
+};
